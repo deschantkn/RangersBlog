fc14746997fec2cc28144223c90245c0091f29c9
alain burindi <alainburindi62@gmail.com>
2019-05-23 15:02:59 +0200

    allow user to edit or delete their own comment

--- ui/js/read.js
+++ ui/js/read.js
@@ -5,7 +5,10 @@ for (let i = 0; i < 5; i++) {
     const content = document.createElement('div');
     const dateHolder = document.createElement('p');
     const commentHolder = document.createElement('div');
-
+    const del = document.createElement('i');
+    const iconHolder = document.createElement('div');
+    const edit = document.createElement('i');
+    
     const text = `Contrary to popular belief, Lorem Ipsum is not simply random text. It has 
     roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. 
     Richard McClintock, a Latin professor at Hampden-Sydney`;
@@ -14,12 +17,17 @@ for (let i = 0; i < 5; i++) {
     content.classList += 'comment_text';
     dateHolder.classList += 'date';
     commentHolder.classList += 'comment';
-
     imageHolder.src = 'images/generic.png';
+    del.classList += 'fas fa-trash-alt';
+    edit.classList += 'far fa-edit';
+
     content.appendChild(document.createTextNode(text));
     dateHolder.appendChild(document.createTextNode(date));
     commentHolder.appendChild(imageHolder);
     commentHolder.appendChild(content);
     content.appendChild(dateHolder);
+    iconHolder.appendChild(del);
+    iconHolder.appendChild(edit);
+    commentHolder.appendChild(iconHolder);
     commentsContainer.appendChild(commentHolder);
 }
