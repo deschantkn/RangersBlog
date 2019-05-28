30882176c23d52d21846e430169abe4b9830c85c
Emmanuel Rukundo <emmanuelrukundo@Emmanuels-MacBook-Pro-2.local>
2019-05-28 16:13:23 +0200

    add get all articles and specific article

--- server/controllers/articles.js
+++ server/controllers/articles.js
@@ -1,11 +1,30 @@
-import models from '../models';
+import models from "../models";
 
 export default {
   create: (req, res) => {
     const { title, content } = req.body;
     models.Article.create({
       title,
-      content,
+      content
     }).then(article => res.json(article));
   },
-};
+
+  list: (req, res) => {
+    return models.Article.findAll().then(articles =>
+      res.status(200).send(articles)
+    );
+  },
+  getArticle: (req, res) =>{
+     return models.Article.findOne({
+       where: {id: req.params.id}
+     }).then(articles => {if(!articles) {
+      return res.status(404).send({
+        message: 'Article Not Found',
+      }); } res.status(200).send(articles);
+     }).catch((error) => res.status(400).send({
+       message:"bad request!"
+     }));
+
+    }
+
+  }
--- server/routes/articlesRouter.js
+++ server/routes/articlesRouter.js
@@ -5,8 +5,10 @@ import Validator from '../middleware/validator';
 
 const articlesRouter = Router();
 
-const { create } = articlesController;
+const { create,list,getArticle } = articlesController;
 
+articlesRouter.get('/:id', getArticle); 
 articlesRouter.post('/create', Validator.schemaCreate, create);
+articlesRouter.get('/', list); // API route for user to get all books in the database
 
 export default articlesRouter;
