0311f6dc8f54803a020374aa715143e439c0d42c
murediane <murediana@gmail.com>
2019-05-29 14:48:14 +0200

    signup endpoint -166150990

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,11 +1,12 @@
 import jwt from 'jsonwebtoken';
+import { hashSync, genSaltSync } from 'bcrypt';
 import bcrypt from 'bcrypt';
 import models from '../models';
+
 
 const { User } = models;
 
 export default {
-  signup: () => {},
   signin: (req, res) => {
     // Look up user in db
     User.findByEmail(req.body.email).then((user) => {
@@ -28,4 +29,31 @@ export default {
       return res.status(401).json({ message: 'Password is invalid' });
     });
   },
+  signup: async (req, res) => {
+    try {
+      const { names, email, password } = req.body;
+
+      const salt = await genSaltSync(parseFloat(process.env.BCRYPT_HASH_ROUNDS) || 10);
+      const hashPassword = await hashSync(password, salt);
+      const user = await models.User.create({
+        names,
+        email,
+        password: hashPassword,
+      });
+
+      res.status(201).json({
+        message: 'Account successfully created',
+        user: {
+          id: user.id,
+          email: user.email,
+        },
+      });
+    } catch (error) {
+      res.status(Error).json({
+        message: 'Sorry, this is not working properly.',
+      });
+    }
+  },
+
+  
 };
--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import authController from '../controllers/auth';
+import Validator from '../middleware/validator';
 
 const authRouter = Router();
 const { signin, signup } = authController;
 
-authRouter.post('/signup', signup);
+authRouter.post('/signup', Validator.userCreate, signup);
 authRouter.post('/signin', signin);
 
 export default authRouter;
