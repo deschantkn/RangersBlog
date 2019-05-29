f3aae7e0d5a5f0ef01ffb63aa87b2689468ee9e2
Deschant Kounou <deschant.kounou@andela.com>
2019-05-29 17:27:41 +0200

    Follow a user

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -4,6 +4,27 @@ const { User } = models;
 
 export default {
   follow: (req, res) => {
+    const { id: currentUser } = req.userData;
+
     // Find user to follow
+    User.findByPk(req.params.userId).then((userToFollow) => {
+      if (!userToFollow) {
+        return res.status(404).json({ status: 404, message: 'User to follow not found' });
+      }
+
+      const { dataValues } = userToFollow;
+
+      // add current user to userToFollow's followers array
+      dataValues.followers.push(currentUser);
+
+      // update data
+      User.update(
+        { followers: dataValues.followers },
+        { where: { id: req.params.userId } },
+      ).then((savedUser) => {
+        // return response
+        return res.status(200).json({ message: `You are now following ${savedUser.dataValues.id}` });
+      });
+    });
   },
 };
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 import userController from '../controllers/users';
+import checkAuth from '../middleware/checkauth';
 
 const userRouter = Router();
 const { follow } = userController;
 
-userRouter.get('/follow/:userId', follow);
+userRouter.get('/follow/:userId', checkAuth, follow);
