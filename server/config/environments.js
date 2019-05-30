f7f7494e57cfdd645b70ad4be7c12764e854c572
Deschant Kounou <deschant.kounou@andela.com>
2019-05-30 18:04:41 +0200

    Add enviroment managment

---
+++ server/config/environments.js
@@ -0,0 +1,28 @@
+import dotenv from 'dotenv';
+/**
+ * Create and export environment variables
+ */
+dotenv.config();
+const environments = {};
+
+environments.dev = {
+  dbname: process.env.DB_NAME,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+};
+
+environments.test = {
+  dbname: process.env.TEST_DB_NAME,
+  dbUser: process.env.TEST_DB_USER,
+  dbPassword: process.env.TEST_DB_PASSWORD,
+};
+
+// Determine which environment we are in
+const currentEnvironment = typeof (process.env.NODE_ENV) === 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+// Check that the current environment is one the envs defined above, if not default to dev
+const environment = typeof (environments[currentEnvironment]) === 'object'
+  ? environments[currentEnvironment] : environments.development;
+
+// Export the module
+export default environment;
